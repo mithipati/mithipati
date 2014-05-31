6660690fd3065759f2e1ae25338dfd5d45c38795
Neil Mithipati <neil.mithipati@gmail.com>
2014-05-30 20:39:09 -0700

    Add helper method to render S3 images remotely

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+  def get_image(name)
+    base_url = 'https://s3-us-west-1.amazonaws.com/mithipati/Portfolio/'
+    name_length = name.length;
+    if name[name_length - 1] == 'w'
+      new_name = name + '.png'
+    else
+      new_name = name + '.jpg'
+    end
+    base_url += new_name
+  end
 end
